4c8678abc26b9857bb9873243a6eab5121ff3ad2
ole-t <56708472+ole-t@users.noreply.github.com>
2023-08-01 11:47:10 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import express from "express"; // для работы этоq команды в файле "package.json" добавлена строка:     "type": "module",
 import fileUpload from 'express-fileupload';
 import cors from "cors";
@@ -8,10 +7,15 @@ import cookieParser from "cookie-parser";
 import m_errorsMiddleWare from './m_MiddleWares/m_errorsMiddleWare.js';
 import mMiddleWare_assessTokenControl from './m_MiddleWares/mMiddleWare_assessTokenControl.js';
 //=======================
-const PORT = 5075;
 const mServer = express();
-//const mDBmongoose_url = 'mongodb+srv://all_users:all_users@cluster0.bfmbg.mongodb.net/?retryWrites=true&w=majority';
 
+// === Тут даныые для работы HTTPS протокола
+import https from 'https';
+import fs from 'fs';
+// import path from 'path'; 
+// === Завершение даныых для работы HTTPS протокола
+
+//const mDBmongoose_url = 'mongodb+srv://all_users:all_users@cluster0.bfmbg.mongodb.net/?retryWrites=true&w=majority';
 
 // для отправки клиенту данных для Куки, мы в нашем проекте передаем туда рефреш-токен
 mServer.use(cookieParser());
@@ -24,7 +28,6 @@ mServer.use(cors());
 // ВАЖНО- cвои МидлВары размещаем перед роутером для своей проверки валидности данных
 mServer.use(mMiddleWare_assessTokenControl);
 
-
 // эта (своя) функция обрабатывает входящие запросы от клиента, используя наши функции обработки из файла 'router.js
 mServer.use(m_Router);
 // функция раздачи статических фалов. 'static'-адрес статической папки в корневом каталоге сервера
@@ -36,24 +39,45 @@ mServer.use(express.static('static'));
 // MiddleWare для обработки ошибок -  длжен располагаться последним в цепочке всех MiddleWare
 mServer.use(m_errorsMiddleWare);
 
+// предварительно создаем SSL-сервер для HTTPS запросов
+const m_SSL_server = https.createServer(
+    {
+        key: fs.readFileSync('./m_SLL_sertificate/key.pem', 'utf8'),
+        cert: fs.readFileSync('./m_SLL_sertificate/cert.pem', 'utf8')
+        /* 
+        key: fs.readFileSync(path.join(__dirname, 'm_SLL_sertificate', 'key.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'm_SLL_sertificate', 'cert.pem'))
+         */
+    },
+    mServer
+)
 
+const PORT = 5075;
 async function mStartApp() {
+    
+        try {
+            //await mongoose.connect(mDBmongoose_url);
+            m_SSL_server.listen(PORT, () => { console.log("Server  is start +SSL  " + PORT) });
+        }
+        catch (error) {
+            console.log("m_ Ошибка сервера:");
+            console.log(error);
+        }
+         
+
+/* 
     try {
         //await mongoose.connect(mDBmongoose_url);
-        mServer.listen(PORT, () => { console.log("Server is start  " + PORT) });
+        mServer.listen(PORT, () => {
+            console.clear();
+            console.log("Server is start  " + PORT)
+        });
     }
     catch (error) {
         console.log("m_ Ошибка сервера:");
         console.log(error);
     }
+ */
+
 }
-
-
-
-
-
-// =========================
 mStartApp(); // запускаем сервер
-
-// использовать команду:
-//   npm run dev
